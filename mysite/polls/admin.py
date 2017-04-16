36287afee0a6f9282bb957e0ca7417c59c7e333f
Melanie Pena <penamelanie3@gmail.com>
2017-04-16 02:36:49 +0000

    made admin page look better

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,5 +3,14 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['question_text']}),
+        ('Update Date Information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+admin.site.register(Question, QuestionAdmin)
