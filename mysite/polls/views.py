d5f74fd3b845e4131c3af31da4ef0cd1e141e979
Melanie Pena <penamelanie3@gmail.com>
2017-04-16 00:36:45 +0000

    got questions to appear as list using bullet points

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import time
 from .models import Question
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', ' .join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
     #return HttpResponse("Hello, world! " + time.strftime("%c"))
     
 def detail(request, question_id):
