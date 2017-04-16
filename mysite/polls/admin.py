96934f7ded4ba10c6f60b64a19890f02b0e68876
Melanie Pena <penamelanie3@gmail.com>
2017-04-16 02:46:08 +0000

    finished all admin, model, and controller requirements (not sure about controller, actually)

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,14 +3,15 @@ from .models import Question, Choice
 
 # Register your models here.
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
+    extra = 1
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,  {'fields': ['question_text']}),
+        ('Update Question Text',  {'fields': ['question_text']}),
         ('Update Date Information', {'fields': ['pub_date'], 'classes':['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
 admin.site.register(Question, QuestionAdmin)
