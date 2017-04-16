86a96477410b1d8b7127dd258caa91b4ab2babeb
Melanie Pena <penamelanie3@gmail.com>
2017-04-16 00:29:42 +0000

    just kidding, now questions show up on the /polls/ page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import time
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world! " + time.strftime("%c"))
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', ' .join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+    #return HttpResponse("Hello, world! " + time.strftime("%c"))
     
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -16,3 +20,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+    
